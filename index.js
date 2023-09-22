e3a50de032ef6743b6abf1084a1f0aaf0e88e6b3
sjy2335 <106982330+sjy2335@users.noreply.github.com>
2023-09-23 01:19:13 +0900

    0923-0119-aggressive message detect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
 import mongoose from "mongoose";
 import resolvers from "./graphql/resolvers";
+import { createProxyMiddleware } from "http-proxy-middleware"; // Import the proxy middleware
 
 mongoose.Promise = global.Promise;
 
@@ -27,4 +28,17 @@ const server = new GraphQLServer({
   resolvers,
   context: { pubsub: resolvers.pubsub },
 });
+
+// Define and configure the proxy middleware
+const proxyMiddleware = createProxyMiddleware("/sentiment-api", {
+  target: "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/sentiment-api": "", // Remove the "/sentiment-api" prefix
+  },
+});
+
+// Add the proxy middleware to the server
+server.express.use("/sentiment-api", proxyMiddleware);
+
 server.start(() => console.log("Server is running on localhost:4000"));
